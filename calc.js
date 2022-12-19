b4c69fcfd262e86db734f62fea581950837a202c
Jitendra Choudhary <jituchoudhary20@gmail.com>
2022-12-19 13:30:42 +0530

    almost working

--- calc.js
+++ calc.js
@@ -1,1 +1,19 @@
-const ele = document.querySelectorAll("button");
+let ele = document.querySelectorAll('.btn');
+let string ='';
+Array.from(ele).forEach((button)=>{
+    button.addEventListener('click',(event)=>{
+    if(event.target.innerHTML=='='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+    }
+    else if(event.target.innerHTML=='clr'){
+        string = '';
+        document.querySelector('input').value = string;
+
+    }
+    else{
+        string = string + event.target.innerHTML;
+        document.querySelector('input').value = string;
+    }
+    })
+})
